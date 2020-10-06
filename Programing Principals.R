9364b6f5955b276f273919210bca7630694369f1
Monika Spreitzer <39218558+MLSpreitzer@users.noreply.github.com>
2020-10-06 08:25:37 -0500

    Add files via upload

---
+++ Programing Principals.R
@@ -0,0 +1,59 @@
+#This project will outline basic programming principals
+
+#Learning variables
+#Integer
+x <- 2L
+#check type to verify integer
+typeof(x)
+
+#double (number with decimal)
+y <- 2.5
+typeof(y)
+
+#complex number
+z <- 3 + 2i
+typeof(z)
+
+#Character (text)
+a <-"h"
+typeof(a)
+
+#logical
+q <- T
+typeof(q)
+q2 <- FALSE
+typeof(q2)
+
+# -----------------------------------------
+# Math operations
+
+#create variable A & B
+A <- 10
+B <- 5
+
+#Sum A + B
+C <- A + B #15
+#print variable value
+C #15
+
+#create 2 new variables
+var1 <- 2.5
+var2 <- 4
+
+#divide var1 by var2
+results <- var1 / var2 #0.625
+
+#square root of variable
+answer <- sqrt(var2) #2
+
+#character operations
+greeting <- "Hello"
+name <- "Bob"
+
+# combine greeting + name (note use of comma not addition sign)
+message <- paste(greeting, name) #"Hello Bob"
+
+
+
+
+
